f4045be6110b6fba6667870e8d6fe35f37172e43
koganiurii <koganiurii@gmail.com>
2020-10-02 17:05:48 +0300

    refactoring containers / styling

--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import MovieCard from "./MovieCard";
 
 const SearchMovie = () => {
   // states - input query, movies
@@ -43,23 +44,7 @@ const SearchMovie = () => {
         {movies
           .filter(movie => movie.poster_path)
           .map(movie => (
-            <div className="card" key={movie.id}>
-              <img
-                className="card--image"
-                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                alt={`${movie.title} poster`}
-              />
-              <div className="card--content">
-                <h3 className="card--title">{movie.title}</h3>
-                <p>
-                  <small>Release Date: {movie.release_date}</small>
-                </p>
-                <p>
-                  <small>Rating: {movie.vote_average}</small>
-                </p>
-                <p className="card--desc">{movie.overview}</p>
-              </div>
-            </div>
+            <MovieCard movie={movie} />
           ))}
       </div>
     </div>
