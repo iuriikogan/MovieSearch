9b3712b23fd3d2e12dcc3d4189796183b5eff7a6
koganiurii <koganiurii@gmail.com>
2020-10-04 14:56:29 +0300

    login/logout buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import SearchMovie from "./components/SearchMovie";
+import Profile from "./components/Profile";
 import LoginButton from "./auth/LoginButton";
 import LogoutButton from "./auth/LogoutButton";
 import { useAuth0 } from "@auth0/auth0-react";
@@ -10,8 +11,9 @@ function App() {
   return (
     <div className="container">
       <h1 className="title">Movie Search</h1>
-      {!isAuthenticated ? <LoginButton /> : <LogoutButton />}
+      {isAuthenticated ? <LogoutButton /> : <LoginButton />}
       <SearchMovie />
+      <Profile />
     </div>
   );
 }
