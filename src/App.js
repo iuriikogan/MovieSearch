8137a4bea23da0d95e7aba47a69212803c170a94
koganiurii <koganiurii@gmail.com>
2020-10-04 14:49:56 +0300

    add auth0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import SearchMovie from "./components/SearchMovie";
+import LoginButton from "./auth/LoginButton";
+import LogoutButton from "./auth/LogoutButton";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isAuthenticated } = useAuth0;
   return (
     <div className="container">
       <h1 className="title">Movie Search</h1>
+      {!isAuthenticated ? <LoginButton /> : <LogoutButton />}
       <SearchMovie />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Auth0Provider
+    domain="iuriikogan.eu.auth0.com"
+    clientId="SftceVinFr0nl427wF7j8N8DrrMxtT0h"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
